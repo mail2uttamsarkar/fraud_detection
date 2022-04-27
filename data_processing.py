a92e4f94c917294c6ce858b075d723c8cb60a316
mail2uttamsarkar <mail2uttamsarkar@gmail.com>
2022-04-27 22:08:58 +0530

    Adding a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,10 @@
 
 print("Adding all the data procrssing ciode related frayd detection")
+
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return a
+
+print("added a new function sum which is not correct")
